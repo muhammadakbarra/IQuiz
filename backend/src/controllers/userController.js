88ad19ac572c72dd49c32b197070936ed83fd5dd
Muhammad Akbar <muhakbar1141@gmail.com>
2024-06-17 02:18:00 +0800

    Fetch: Login, token JWT, change Password

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
 
 exports.createUser = async (req, res) => {
   const {
@@ -38,6 +40,14 @@ exports.getUser = async (req, res) => {
   }
 };
 
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 exports.updateUser = async (req, res) => {
   const { fullname, email, description, profilePhoto } = req.body;
   try {
@@ -65,3 +75,71 @@ exports.deleteUser = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+exports.loginUser = [
+  body("username").notEmpty().withMessage("Username is required"),
+  body("password").notEmpty().withMessage("Password is required"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ where: { Username: username } });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.Password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+      const payload = {
+        id: user.UserId,
+        username: user.Username,
+        email: user.Email,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.json({ token });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+];
+
+// ubah sandi
+exports.changePassword = [
+  body("oldPassword").notEmpty().withMessage("Old password is required"),
+  body("newPassword")
+    .isLength({ min: 6 })
+    .withMessage("New password must be at least 6 characters long"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { oldPassword, newPassword } = req.body;
+    try {
+      const user = await User.findByPk(req.user.id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(oldPassword, user.Password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Old password is incorrect" });
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.Password = hashedPassword;
+      await user.save();
+
+      res.json({ message: "Password changed successfully" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+];
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/users", userController.createUser);
-router.get("/users/:id", userController.getUser);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+router.post("/login", userController.loginUser); // Route untuk login
+router.get("/users/:id", authMiddleware, userController.getUser); // Protected route
+router.get("/users", authMiddleware, userController.getAllUsers);
+router.put("/users/:id", authMiddleware, userController.updateUser); // Protected route
+router.delete("/users/:id", authMiddleware, userController.deleteUser); // Protected route
+router.post("/change-password", userController.changePassword);
 
 module.exports = router;
